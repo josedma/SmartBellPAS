c6281ed1d3cab853b644b3d3c4f2ea3e9aac8b43
Saul Machado <saulmachado1997@hotmail.com>
2017-11-14 09:47:52 -0500

    s

--- Timbre/consulta.php
+++ Timbre/consulta.php
@@ -8,8 +8,8 @@ $consulta= "SELECT * FROM usuarios WHERE usuario='$nombre' AND contrasena='$pass
 $resultado= $con->query($consulta);
 
 	if ($resultado->num_rows>0) {
-		
 		include_once("inicio.html");
+
 	}else{
 		echo '<script>alert("Datos incorrectos, Intentalo Nuevamente")</script>';
 		include "index.html";
