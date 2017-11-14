584be12f34505fb02ad623ff1259c594062d68d8
Saul Machado <saulmachado1997@hotmail.com>
2017-11-14 09:39:21 -0500

    rename

--- Timbre/consulta.php
+++ Timbre/consulta.php
@@ -9,7 +9,7 @@ $resultado= $con->query($consulta);
 
 	if ($resultado->num_rows>0) {
 		
-		include "menu.html";
+		include "inicio.html";
 	}else{
 		echo '<script>alert("Datos incorrectos, Intentalo Nuevamente")</script>';
 		include "index.html";
